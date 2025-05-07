ccd1fabaf96540342700a734f86d74561f9abcce
MaheshPaade <116006160+mahesh2i3u893y1y@users.noreply.github.com>
2025-05-07 17:52:17 +0530

    Update reciept.js

--- src/routers/reciept.js
+++ src/routers/reciept.js
@@ -23,19 +23,12 @@ receiptRouter.get('/repayments/receipt/:id', authMiddleWare, async (req, res) =>
     const shopkeeper = await User.findById(shopkeeperId);
     const Customer = Loan.customerId;
 
-    // Load HTML template
+   
     const templatePath = path.join(__dirname, '../templates/template.html');
     let html = fs.readFileSync(templatePath, 'utf8');
 
 
-    // const backgroundImagePath = path.join(__dirname, "../../assets", "pronoia.png");
-    // const backgroundImageBase64 = fs.readFileSync(backgroundImagePath, { encoding: "base64" });
-    // const logoDataUri = `data:image/png;base64,${backgroundImageBase64}`;
-
-    // Build logo URL
-    // const logoUrl = `${req.protocol}://${req.get('host')}/assets/logo.png`;
-
-    // Replace placeholders
+    
     html = html.replace('{{shopkeeperName}}', shopkeeper.name)
       .replace('{{shopkeeperEmail}}', shopkeeper.email)
       .replace('{{customerName}}', Customer.name)
@@ -46,26 +39,24 @@ receiptRouter.get('/repayments/receipt/:id', authMiddleWare, async (req, res) =>
       .replace('{{amountPaid}}', Repayment.amount.toFixed(2))
       .replace('{{remainingAmount}}', Loan.remainingAmount.toFixed(2))
       .replace('{{paymentDate}}', new Date(Repayment.date).toLocaleString())
-    //   .replace('{{logoUrl}}', logoUrl);
 
-    // Generate PDF using Puppeteer
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setContent(html);
     const pdfBuffer = await page.pdf({ format: 'A4' });
     await browser.close();
 
-    // Ensure Repayments folder exists
+    
     const outputDir = path.join(__dirname, "../../Repayments");
     if (!fs.existsSync(outputDir)) {
       fs.mkdirSync(outputDir);
     }
 
-    // Write PDF file
+  
     const filePath = path.join(outputDir, `receipt-${repaymentId}.pdf`);
     fs.writeFileSync(filePath, pdfBuffer);
 
-    // Send file as response
+  
     res.download(filePath);
 
   } catch (err) {
